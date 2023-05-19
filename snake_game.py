130816c7e769d66ecb5741448209296ab2d8a0fc
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-05-19 21:31:18 +0700

    optimize code

--- snake_game.py
+++ snake_game.py
@@ -36,14 +36,14 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Rắn săn mồi")
 
 # Load images
-pause_image = pygame.transform.scale(
-    pygame.image.load('pause.png'), (BUTTON_SIZE, BUTTON_SIZE))
-play_image = pygame.transform.scale(
-    pygame.image.load('play.png'), (BUTTON_SIZE, BUTTON_SIZE))
+images = {
+    "pause": pygame.transform.scale(pygame.image.load('pause.png'), (BUTTON_SIZE, BUTTON_SIZE)),
+    "play": pygame.transform.scale(pygame.image.load('play.png'), (BUTTON_SIZE, BUTTON_SIZE))
+}
 
 # Game state variables
 paused = False
-button_image = pause_image
+button_image = images["pause"]
 button_rect = pygame.Rect(140, 70, BUTTON_SIZE, BUTTON_SIZE)
 message = None
 message_start_time = None
@@ -56,67 +56,42 @@ obstacles = []
 def draw_border():
     pygame.draw.rect(screen, BLACK, pygame.Rect(*GAME_AREA), BORDER_WIDTH)
 
+
 def random_position(snake=None, avoid=None):
     if avoid is None:
         avoid = set()
     while True:
-        position = (
-            random.randint(GAME_AREA[0] // GRID_SIZE + 1, (GAME_AREA[0] +
-                           GAME_AREA[2]) // GRID_SIZE - 2) * GRID_SIZE,
-            random.randint(GAME_AREA[1] // GRID_SIZE + 1, (GAME_AREA[1] +
-                           GAME_AREA[3]) // GRID_SIZE - 2) * GRID_SIZE
-        )
-
+        x = random.randrange(GAME_AREA[0] // GRID_SIZE + 1, (GAME_AREA[0] + GAME_AREA[2]) // GRID_SIZE - 2) * GRID_SIZE
+        y = random.randrange(GAME_AREA[1] // GRID_SIZE + 1, (GAME_AREA[1] + GAME_AREA[3]) // GRID_SIZE - 2) * GRID_SIZE
+        position = (x, y)
         if snake is None or (position not in snake and position not in avoid):
             return position
+        
 
-
-def draw_text(text, font_name, font_size, color, center):
-    font = pygame.font.Font(font_name, font_size)
+def draw_text(text, font, size, color, center):
     text_render = font.render(text, True, color)
-    text_rect = text_render.get_rect()
-    text_rect.center = center
+    text_rect = text_render.get_rect(center=center)
     screen.blit(text_render, text_rect)
 
 def draw_score(score, high_score, play_time, special_food_time_left):
-    font1 = pygame.font.SysFont('Arial', 36)
-    text1 = font1.render(f"Score: {score}", True, BLACK)
-    text1_rect = text1.get_rect()
-    text1_rect.topleft = (10, 10)
-    screen.blit(text1, text1_rect)
-
+    draw_text(f"Score: {score}", font1, 36, BLACK, (70, 30))
+    high_score_text = f"High Score: {high_score}"
     if score > high_score:
         high_score = score
-
-    font2 = pygame.font.SysFont('Arial', 36, bold=True)
-    text2 = font2.render(f"High Score: {high_score}", True, BLACK)
-    text2_rect = text2.get_rect()
-    text2_rect.topright = (WINDOW_SIZE[0] - 10, 10)
-    screen.blit(text2, text2_rect)
-
-    font3 = pygame.font.SysFont('Arial', 36)
-    text3 = font3.render(f"Time: {play_time} s", True, BLACK)
-    text3_rect = text3.get_rect()
-    text3_rect.topleft = (340, 10)
-    screen.blit(text3, text3_rect)
-
+        save_high_score(high_score)
+    draw_text(high_score_text, font2, 36, BLACK, (WINDOW_SIZE[0] - 130, 30))
+    draw_text(f"Time: {play_time} s", font3, 36, BLACK, (370, 30))
     if special_food_time_left > 0:
-        font4 = pygame.font.SysFont('Arial', 36)
-        text4 = font4.render(f"Special food: {int(special_food_time_left)} s", True, SPECIAL_FOOD_COLOR)
-        text4_rect = text4.get_rect()
-        text4_rect.bottomleft = (300, 550)
-        screen.blit(text4, text4_rect)
+        draw_text(f"Special food: {int(special_food_time_left)} s", font4, 36, SPECIAL_FOOD_COLOR, (300, 550))
 
 def draw_level(level):
-    font = pygame.font.SysFont('Arial', 36)
-    text = font.render(f"Level: {level}", True, BLACK)
-    text_rect = text.get_rect()
-    text_rect.topleft = (160, 10)
-    screen.blit(text, text_rect)
+    draw_text(f"Level: {level}", font, 36, BLACK, (220, 30))
+
 
 def draw_obstacles():
     for obstacle in obstacles:
         pygame.draw.rect(screen, BLACK, pygame.Rect(*obstacle, SNAKE_SIZE, SNAKE_SIZE))
+
 
 def next_level(level):
     global current_level, obstacles, message, message_start_time, snake
@@ -149,17 +124,14 @@ def next_level(level):
     message = f"Moving to Level {level}!"
     message_start_time = time.time()
 
-    draw_text(message, None, 54, RED, (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2))
+    draw_text(message, font, 54, RED, (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2))
     pygame.display.flip()
-    time.sleep(2)
+    pygame.time.delay(2000)
 
 def game_over(score):
-    draw_text("Game Over!", None, 54, RED,
-              (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2))
-    draw_text(f"Your Score: {score}", None, 36, BLACK,
-              (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2 + 60))
-    draw_text("Press 'R' to play again or 'Q' to quit.", None, 24,
-              BLACK, (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2 + 100))
+    draw_text("Game Over!", font, 54, RED, (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2))
+    draw_text(f"Your Score: {score}", font, 36, BLACK, (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2 + 60))
+    draw_text("Press 'R' to play again or 'Q' to quit.", font, 24, BLACK, (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2 + 100))
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
@@ -170,11 +142,7 @@ def game_over(score):
                 main()
 
 def draw_paused():
-    font = pygame.font.Font(None, 54)
-    text = font.render("Game Paused", True, RED)
-    text_rect = text.get_rect()
-    text_rect.center = (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2)
-    screen.blit(text, text_rect)
+    draw_text("Game Paused", font, 54, RED, (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2))
 
 def get_high_score():
     try:
@@ -196,8 +164,7 @@ def main():
     global button_image, paused, special_food, special_food_start_time, food_eaten, current_level, message, message_start_time
     start_time = time.time()
     head = random_position()
-    snake = [head, (head[0] - SNAKE_SIZE, head[1]),
-             (head[0] - 2 * SNAKE_SIZE, head[1])]
+    snake = [head, (head[0] - SNAKE_SIZE, head[1]), (head[0] - 2 * SNAKE_SIZE, head[1])]
     snake_direction = (SNAKE_SPEED, 0)
     new_direction = snake_direction
     food = random_position(snake)
@@ -220,16 +187,15 @@ def main():
                     }
                     new_direction = direction_map.get(event.key, snake_direction)
                     moved = False
-
             if event.type == KEYDOWN and event.key == K_p or (event.type == MOUSEBUTTONDOWN and event.button == 1 and button_rect.collidepoint(event.pos)):
                 paused = not paused
-                button_image = play_image if paused else pause_image
-            screen.fill((255, 255, 255))
-
+                button_image = images["play" if paused else "pause"]
+            screen.fill(WHITE)
+        
         screen.blit(button_image, button_rect)
-
+        
         if paused:
-            screen.blit(play_image, button_rect)
+            screen.blit(images["play"], button_rect)
             draw_paused()
             pygame.display.flip()
             continue
@@ -239,9 +205,8 @@ def main():
         snake.insert(0, head)
         moved = True
 
-        if (head[0] < GAME_AREA[0] or head[0] >= GAME_AREA[0] + GAME_AREA[2] or
-            head[1] < GAME_AREA[1] or head[1] >= GAME_AREA[1] + GAME_AREA[3] or
-            head in snake[1:]):
+        if not (GAME_AREA[0] <= head[0] < GAME_AREA[0] + GAME_AREA[2] and
+                GAME_AREA[1] <= head[1] < GAME_AREA[1] + GAME_AREA[3]) or head in snake[1:]:
             update_high_score(score)
             game_over(score)
             pygame.display.flip()
@@ -260,8 +225,7 @@ def main():
         if score >= 15 and current_level <= score // 15:
             next_level(current_level + 1)
             head = random_position(avoid=obstacles)
-            snake = [head, (head[0] - SNAKE_SIZE, head[1]),
-                    (head[0] - 2 * SNAKE_SIZE, head[1])]
+            snake = [head, (head[0] - SNAKE_SIZE, head[1]), (head[0] - 2 * SNAKE_SIZE, head[1])]
             food = random_position(snake)
 
         if current_level > 1:
@@ -286,22 +250,24 @@ def main():
 
         screen.fill(WHITE)
         draw_border()
-        for segment in snake:
+        pygame.draw.rect(screen, GREEN, pygame.Rect(*head, SNAKE_SIZE, SNAKE_SIZE))
+        for segment in snake[1:]:
             pygame.draw.rect(screen, GREEN, pygame.Rect(*segment, SNAKE_SIZE, SNAKE_SIZE))
         pygame.draw.rect(screen, RED, pygame.Rect(*food, SNAKE_SIZE, SNAKE_SIZE))
-
         if special_food:
             pygame.draw.rect(screen, BLUE, pygame.Rect(*special_food, SNAKE_SIZE, SNAKE_SIZE))
-
         if current_level > 1:
             draw_obstacles()
-
         draw_score(score, high_score, play_time, special_food_time_left)
         draw_level(current_level)
-
-        screen.blit(pause_image, button_rect)
+        screen.blit(images["pause"], button_rect)
         pygame.display.flip()
         fps.tick(7)
 
 if __name__ == "__main__":
+    font1 = pygame.font.SysFont('Arial', 36)
+    font2 = pygame.font.SysFont('Arial', 36, bold=True)
+    font3 = pygame.font.SysFont('Arial', 36)
+    font4 = pygame.font.SysFont('Arial', 36)
+    font = pygame.font.SysFont('Arial', 36)
     main()
